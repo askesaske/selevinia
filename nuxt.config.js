84a718fcad356ab4d4e7f4105e004472902c7ad7
Orazalinov Askhat <58249226+askesaske@users.noreply.github.com>
2022-06-10 20:01:32 +0600

    favicon changed

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,7 @@ export default {
       { name: 'csrf-token', content: '{{ csrf_token() }}' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/icon.png' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
 
